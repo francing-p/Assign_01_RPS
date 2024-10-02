cd0a1752ebf87aa4753dbb4d0599b77e415d03cc
francing-p <ngfranciscus@gmail.com>
2024-10-02 08:05:26 -0400

    Commit

--- src/Game.java
+++ src/Game.java
@@ -9,48 +9,48 @@ public class Game {
         String end="";
 
 
-        do {
-            do {
-                System.out.print("Player A. Enter your move using R,P, or S: ");
+        do { // outside loop that restarts the entire thing when 'Y' or 'y' is entered
+            do { // runs until receiving r,p,s or R,P,S
+                System.out.print("Player A. Enter your move using [R/P/S] or [r/p/s]: ");
                 moveA = in.nextLine();
             }
-            while(!moveA.equals("R") && !moveA.equals("P") && !moveA.equals("S"));
-            do {
-                System.out.print("Player B. Enter your move using R, P, or S: ");
+            while(!moveA.equalsIgnoreCase("R") && !moveA.equalsIgnoreCase("P") && !moveA.equalsIgnoreCase("S"));
+            do { // runs until receiving r,p,s or R,P,S
+                System.out.print("Player B. Enter your move using [R/P/S] or [r/p/s]: ");
                 moveB = in.nextLine();
             }
-            while(!moveB.equals("R") && !moveB.equals("P") && !moveB.equals("S"));
-            if (moveA.equals("R")) {
-                if (moveB.equals("R")) {
+            while(!moveB.equalsIgnoreCase("R") && !moveB.equalsIgnoreCase("P") && !moveB.equalsIgnoreCase("S"));
+            if (moveA.equalsIgnoreCase("R")) { // outer loop for moveA
+                if (moveB.equalsIgnoreCase("R")) { // inner loop #1 for moveB
                     System.out.println("Rock vs. Rock. Tie");
-                } else if (moveB.equals("P")) {
-                    System.out.println("Rock vs. Paper. Player B wins");
-                } else if (moveB.equals("S")) {
-                    System.out.println("Rock vs. Scissors. Player A wins");
+                } else if (moveB.equalsIgnoreCase("P")) {
+                    System.out.println("Paper covers rock. Player B wins");
+                } else if (moveB.equalsIgnoreCase("S")) {
+                    System.out.println("Rock breaks scissors. Player A wins");
                 }
-            } else if (moveA.equals("P")) {
-                if (moveB.equals("R")) {
-                    System.out.println("Paper vs. Rock. Player A wins");
-                } else if (moveB.equals("P")) {
+            } else if (moveA.equalsIgnoreCase("P")) { // outer loop #2 moveA
+                if (moveB.equalsIgnoreCase("R")) { // inner loop #2 for moveB
+                    System.out.println("Paper covers rock. Player A wins");
+                } else if (moveB.equalsIgnoreCase("P")) {
                     System.out.println("Paper vs. Paper. Tie");
-                } else if (moveB.equals("S")) {
-                    System.out.println("Paper vs. Scissors. Player B wins");
+                } else if (moveB.equalsIgnoreCase("S")) {
+                    System.out.println("Scissors cuts paper. Player B wins");
                 }
 
-            } else if (moveA.equals("S")) {
-                if (moveB.equals("R")) {
-                    System.out.println("Scissors vs. Rock. Player B wins");
-                } else if (moveB.equals("P")) {
-                    System.out.println("Scissors vs. Paper. Player A wins");
-                } else if (moveB.equals("S")) {
+            } else if (moveA.equalsIgnoreCase("S")) { // outer loop #3 moveA
+                if (moveB.equalsIgnoreCase("R")) { // inner loop #3 for moveB
+                    System.out.println("Rock breaks scissors. Player B wins");
+                } else if (moveB.equalsIgnoreCase("P")) {
+                    System.out.println("Scissors cuts paper. Player A wins");
+                } else if (moveB.equalsIgnoreCase("S")) {
                     System.out.println("Scissors vs. Scissors. Tie");
                 }
             }
-            System.out.print("Would you like to continue? Y or N: ");
+            System.out.print("Would you like to continue? [Y/N]: ");
             if (in.hasNextLine()) {
                 end = in.nextLine();
             }
         }
-        while(end.equals("Y"));
+        while(end.equalsIgnoreCase("Y")); // continue when 'y' or 'Y' entered
     }
 }
